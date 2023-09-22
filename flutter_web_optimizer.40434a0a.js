caa6454f18504f89727c62c25c4f1e91b0a90c64
yajw <yajw@users.noreply.github.com>
2023-09-22 14:33:02 +0000

    just another update yajw/6@3751d62ded5db9362a1e119349ed0eb9d4d9b6b8

---
+++ flutter_web_optimizer.40434a0a.js
@@ -0,0 +1,103 @@
+    // The code below is injected by flutter web optimizer, do not edit!!!!!!
+    
+    // window.assetBase
+    var assetBase = "https://yajw.org/";
+    
+    // window.mainjsManifest
+    var mainjsManifest = {
+  "main.dart.js": "main.dart.75b1211a.js",
+  "main.dart_0.js": "main.dart_0.4ce1218f.js",
+  "main.dart_1.js": "main.dart_1.48425612.js",
+  "main.dart_2.js": "main.dart_2.d897cc94.js",
+  "main.dart_3.js": "main.dart_3.6ba27f8e.js",
+  "main.dart_4.js": "main.dart_4.86e43d98.js",
+  "main.dart_5.js": "main.dart_5.b2a050b9.js"
+};
+    
+    // window.hashFileManifest
+    var hashFileManifest = {
+  "assets/AssetManifest.json": "assets/AssetManifest.61d24573.json",
+  "assets/FontManifest.json": "assets/FontManifest.f4fda0a2.json",
+  "assets/fonts/MaterialIcons-Regular.otf": "assets/fonts/MaterialIcons-Regular.3265e4dc.otf",
+  "favicon.png": "favicon.5dcef449.png",
+  "flutter.js": "flutter.822458aa.js",
+  "icons/Icon-192.png": "icons/Icon-192.ac9a721a.png",
+  "icons/Icon-512.png": "icons/Icon-512.96e75261.png",
+  "icons/Icon-maskable-192.png": "icons/Icon-maskable-192.c457ef57.png",
+  "icons/Icon-maskable-512.png": "icons/Icon-maskable-512.301a7604.png",
+  "main.dart.js": "main.dart.75b1211a.js",
+  "main.dart_0.js": "main.dart_0.4ce1218f.js",
+  "main.dart_1.js": "main.dart_1.48425612.js",
+  "main.dart_2.js": "main.dart_2.d897cc94.js",
+  "main.dart_3.js": "main.dart_3.6ba27f8e.js",
+  "main.dart_4.js": "main.dart_4.86e43d98.js",
+  "main.dart_5.js": "main.dart_5.b2a050b9.js",
+  "manifest.json": "manifest.f7faff1f.json"
+};
+    
+    // hook dynamic create element `src` `href` property
+    (function(){
+      var _defineProperty = function (element, property) {
+        var value;
+        Object.defineProperty(element, property, {
+          get: function () {
+            return value;
+          },
+          set: function (v) {
+            value = v;
+            if (typeof v === 'string') {
+              if (v.startsWith('./')) {
+                v = v.replace('./', '');
+              }
+              if (hashFileManifest[v]) {
+                value = assetBase + hashFileManifest[v];
+              }
+            }
+            element.setAttribute(property, value);
+          }
+        })
+  
+        var _setAttribute = element.setAttribute;
+        element.setAttribute = function () {
+          var args = Array.prototype.slice.call(arguments);
+          // if (args[0] === 'src') {
+          //   //do something here
+          //   console.log('setAttribute', args[1])
+          // }
+          _setAttribute.apply(element, args);
+        }
+      }
+  
+      var _createElement = document.createElement;
+      var _hookProperties = {'link': 'href', 'script': 'src', 'audio': 'src', 'video': 'src', 'map': 'src', 'img': 'src'};
+      document.createElement = function (tagName) {
+        var element = _createElement.call(document, tagName);
+        var property = _hookProperties[tagName.toLowerCase()];
+        if (property) {
+          _defineProperty(element, property);
+        }
+        return element;
+      }  
+    })();
+
+    // load main.dart.js_xxx.part js
+    function dartDeferredLibraryLoader(uri, successCallback, errorCallback, loadId) {
+      console.info('uri: ' + uri + ', loadId: '+ loadId);
+      var src;
+      try {
+        var url = new URL(uri);
+        var key = url.pathname.replaceAll(/(.*)(main\.dart\.(.+)\.js)/g, '$2');
+        src = assetBase + mainjsManifest[key];
+      } catch (e) {
+        var key = uri.replaceAll(/(.*)(main\.dart\.(.+)\.js)/g, '$2');
+        src = assetBase + mainjsManifest[key];
+      }
+    
+      var script = document.createElement("script");
+      script.type = "text/javascript";
+      script.src = src;
+      script.addEventListener("load", successCallback, false);
+      script.addEventListener("error", errorCallback, false);
+      document.body.appendChild(script);
+    }
+    
