cf34384b225ea865298a28ef69aed5a460da677a
yajw <yajw@users.noreply.github.com>
2023-09-22 15:13:14 +0000

    just another update yajw/6@0cc47a975e014c2c52834556b9f71b2d58fc1626

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -91,8 +91,8 @@ const RESOURCES = {"canvaskit/chromium/canvaskit.js": "9c4f8f68506cf4d2dc9b05219
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "flutter.js": "7d69e653079438abfbb24b82a655b0a4",
-"index.html": "527fcc75947f5974546febc9177d4876",
-"/": "527fcc75947f5974546febc9177d4876",
+"index.html": "bb63bb59b1def26d1397c6fc8392c843",
+"/": "bb63bb59b1def26d1397c6fc8392c843",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "main.dart.js": "0d55023a2cb77e5e890b7a6813174534",
 "manifest.json": "9681b6d51d61be704c1d337100d5c2c1"};
